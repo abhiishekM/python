418e86a4f86066f0d91542ea06b83f9238d636df
abhiishekM <abhishivam285@gmail.com>
2025-01-20 15:16:51 +0530

    Create allprograms.py

---
+++ python/day 1/allprograms.py
@@ -0,0 +1,32 @@
+print("Hey")
+
+print('Hello World')
+
+print("""Hi""")
+
+print('''Hey''')
+
+
+a=5.2
+b=9.6
+print(a+b)
+print(type(a+b))
+
+
+a= "hebcig"
+b="fad"
+print(a+b)
+print(type(a+b))
+
+a=input("Enter first number - ")
+b=input("Enter second number - ")
+print(int(a)+int(b))
+
+
+
+a=int(input("Enter first number - "))
+b=int(input("Enter second number - "))
+print(a+b)
+
+
+
