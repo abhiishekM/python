5e5539bb9993ab5465b84e090310938bf3eeece4
abhiishekM <abhishivam285@gmail.com>
2025-01-21 13:36:51 +0530

    Update day2allprograms.py

--- day2allprograms.py
+++ day2allprograms.py
@@ -1,1 +1,30 @@
+#keywords and identifiers and operators
+a=10
+a+=5
+print(a)
+a-=3
+print(a)
+a*=4
+print(a)
+a//=2
+print(a)
+
+
+a="abhishek mishra"
+print("abhishek" in a)
+
+lst=[1,2,3,"apple"]
+print(1 in lst)
+print(7 not in lst)
+print("apple" in lst)
+
+
+#bitwise operator
+x=5
+y=3
+print(x&y)
+print(x|y)
+print(x^y)
+
+
 
