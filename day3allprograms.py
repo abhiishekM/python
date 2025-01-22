02c78b60ca3a9d7f945cc1f450fe61087fc5211c
abhiishekM <abhishivam285@gmail.com>
2025-01-22 12:20:34 +0530

    Create day3allprograms.py

---
+++ day3allprograms.py
@@ -0,0 +1,50 @@
+x=5
+y=6
+print(x&y)
+print(x|y)
+print(x^y)
+
+
+a="Hello guys "
+print(a*3)
+
+
+a="Hello"
+print(a[-1])
+
+
+
+
+L=[1,2,3,"HI","HELLO"]
+print("The digit is found at index ",L.index(2))
+
+#output -- The digit is found at index  1
+
+l=[1,2,3,4,5,6,7]
+print(l[0:5:1])
+
+s="Hello Python"
+print(s[-1:-13:-1])
+
+print(s[::-1])
+print(s[::2])
+
+
+l=[1,42,53,74,15,6]
+l.sort()
+print(l)
+
+
+l=[4,5,6]
+l[0]=1
+print(l)
+
+
+l=["RAM",45,True]
+l[0]="abhishek"
+print(l)
+
+
+l=[1,2,3,4]
+l.append(5)
+print(l)
