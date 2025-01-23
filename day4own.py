13df1866f18d6da4fbbb450e6b7408f632d16dee
abhiishekM <abhishivam285@gmail.com>
2025-01-23 12:25:34 +0530

    Update day4own.py

--- day4own.py
+++ day4own.py
@@ -1,1 +1,32 @@
+#greatest of two integers
+
+a=int(input("Enter first integer -"))
+b=int(input("Enter second integer -"))
+if a>b:
+  print("A is greater")
+else:
+  print("B is greater")
+
+
+#check two integers are equal or not
+
+a=int(input("Enter first integer -"))
+b=int(input("Enter second integer -"))
+if a==b:
+  print("Both are equal")
+else:
+  print("both are not equal")
+
+
+#greatest of three integer
+
+a=int(input("Enter first integer -"))
+b=int(input("Enter second integer -"))
+c=int(input("Enter third integer - "))
+if a>b and a>c:
+  print("A is greatest")
+elif b>c:
+    print("b is greatest")
+else:
+  print("c is greatest")
 
