406f8645e61e5d47a151ca177f2f94376a5b6afb
abhiishekM <abhishivam285@gmail.com>
2025-01-23 15:23:44 +0530

    Update day4own.py

--- day4own.py
+++ day4own.py
@@ -1,3 +1,73 @@
+print("Enter your choice:\n 1.Addition\n2.Subtraction\n3.Division\n4.Multiplication")
+n=int(input())
+if n==1:
+  print("Enter number 1 and number 2")
+  a=int(input("First number "))
+  b=int(input("Second number "))
+  print("Sum is:",a+b)
+elif n==2:
+  print("Enter number 1 and number 2")
+  a=int(input("First number "))
+  b=int(input("Second number "))
+  print("Subtraction is:",a-b)
+elif n==3:
+  print("Enter number 1 and number 2")
+  a=int(input("First number "))
+  b=int(input("Second number "))
+  print("Division is:",a/b)
+elif n==4:
+  print("Enter number 1 and number 2")
+  a=int(input("First number "))
+  b=int(input("Second number "))
+  print("Product is:",a*b)
+else:
+  print("Not a valid choice")
+
+
+
+print("Enter two numbers")
+a=int(input("First number "))
+b=int(input("Second number "))
+print("Enter your choice:\n1.Addition\n2.Subtraction\n3.Division\n4.Multiplication")
+n=int(input())
+if n==1:
+  print("Sum is:",a+b)
+elif n==2:
+  print("Subtraction is:",a-b)
+elif n==3:
+  print("Division is:",a/b)
+elif n==4:
+  print("Product is:",a*b)
+else:
+  print("Not a valid choice") 
+
+
+
+day=int(input("Enter the week day - "))
+if day==1:
+  print("MONDAY")
+elif day==2:
+  print("TUESDAY")
+elif day==3:
+  print("WEDNESDAY")
+elif day==4:
+  print("THURSDAY")
+elif day==5:
+  print("FRIDAY")
+elif day==6:
+  print("SATURDAY")
+elif day==7:
+  print("SUNDAY")
+else:
+  print("INVALID DAY")
+
+
+
+
+
+
+
+
 #greatest of two integers
 
 a=int(input("Enter first integer -"))
@@ -30,3 +100,10 @@ elif b>c:
 else:
   print("c is greatest")
 
+
+#program to check vowel or consonant
+str1=input('Enter one character')
+if str1 in 'aeiou':
+  print('Vowel')
+else:
+  print('Consonant')
