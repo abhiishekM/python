09714c45a3046817e0d6a298c95723171121de4b
abhiishekM <abhishivam285@gmail.com>
2025-01-25 11:10:18 +0530

    Update day6allprograms.py
    Patterns Day

--- day6allprograms.py
+++ day6allprograms.py
@@ -29,3 +29,28 @@ if pf==n:
   print('Perfect Number')
 else:
   print('Not a perfect number')
+
+## nested loop ##
+
+#table
+for i in range(1,6):
+  for j in range(1,6):
+    print(f"{i} x {j} = {i*j}")
+  print()
+
+####  patterns   ###
+
+n=int(input('Enter no.of rows - '))
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    print('*',end="")
+  print()
+
+
+n=int(input('Enter no.of rows - '))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print('*',end="")
+  print()
+
+
