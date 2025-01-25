162d777c31f7437ec23ff6ebed635634b24af67f
abhiishekM <abhishivam285@gmail.com>
2025-01-25 11:25:32 +0530

    patterns day

--- day6allprograms.py
+++ day6allprograms.py
@@ -54,3 +54,26 @@ for i in range(1,n+1):
   print()
 
 
+n=int(input('Enter no.of rows - '))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(j,end="")
+  print()
+
+
+n=int(input('Enter no.of rows - '))
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(i,end="")
+  print()
+
+
+n=int(input('Enter no.of rows - '))
+num=1
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print(num,end=" ")
+    num+=1
+  print()
+
+
