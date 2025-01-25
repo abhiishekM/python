fb056dc10b00e2e7a0d574c3b1d3a17f19e3db8d
abhiishekM <abhishivam285@gmail.com>
2025-01-25 10:19:05 +0530

    Update day6allprograms.py

--- day6allprograms.py
+++ day6allprograms.py
@@ -1,1 +1,20 @@
+#factors of a number
+num=int(input('Enter the number - '))
+for i in range(1,num):
+  if num%i==0:
+    print(i)
 
+
+#perfect number program
+num=int(input('Enter the number - '))
+L=[]
+pf=0
+for i in range(1,num+1):
+  if num%i==0:
+    L.append(i)
+for i in range(0,len(L)):
+  pf+=i
+if pf==num:
+  print('Perfect Number')
+else:
+  print('Not a perfect number')
