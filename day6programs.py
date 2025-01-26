721fed7d38d8b2887ff3107f5931b2b61a3a0bfd
abhiishekM <abhishivam285@gmail.com>
2025-01-26 08:14:30 +0530

    Update day6programs.py

--- day6programs.py
+++ day6programs.py
@@ -1,1 +1,23 @@
+## user defined functions  ###
 
+def sum(a,b):
+  add=a+b
+  return add
+a=int(input('Enter first number- '))
+b=int(input('Enter second number- '))
+sum(a,b)
+
+def pow(a,b):
+  s=a**b
+  return s
+a=int(input('Enter number- '))
+b=int(input("Enter it's power- "))
+pow(a,b)
+
+
+def sub(a,b):
+  s=a-b
+  return s
+a=int(input('Enter first number- '))
+b=int(input('Enter second number- '))
+sub(a,b)
