a2440f6762017e638515d3fefab7ac9d383a6b79
abhiishekM <abhishivam285@gmail.com>
2025-01-27 10:55:51 +0530

    Update day7programs.py

--- day7programs.py
+++ day7programs.py
@@ -1,1 +1,31 @@
+## lambda function ##
+
+sum=lambda x,y:x+y
+print(sum(1,5))
+
+sub=lambda x,y: x-y
+print(sub(1,4))
+
+
+pow=lambda x,y: x**y
+print(pow(2,4))
+
+
+div=lambda x,y: x/y
+print(div(54,9))
+
+
+a=int(input('Enter first number- '))
+b=int(input('Enter second number- '))
+sum=lambda x,y:x+y
+print('sum is',sum(a,b))
+
+
+mul=lambda x,y: x*y
+print(mul(2,4))
+
+
+evodd=lambda x: "even" if x%2==0 else "odd"
+print(evodd(5))
+
 
