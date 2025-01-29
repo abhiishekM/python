b3557a601210bd724fbef80f4a37819994ba1b84
abhiishekM <abhishivam285@gmail.com>
2025-01-29 14:53:27 +0530

    Update day9porgrams.py

--- day9porgrams.py
+++ day9porgrams.py
@@ -51,3 +51,99 @@ display_info(**packed_kwargs)
 #output- name : Alice
 #output- age : 15
 
+my_list = [1, 2, 3]
+my_list[1] = 20 
+print(my_list) # Output: [1, 20, 3]
+
+
+stri=['hi','heloo','python']
+stri[2]='sujeet'
+print(stri)
+
+
+stri=['hi','heloo','python']
+stri.extend(['bihari','hai'])
+print(stri)
+
+
+ls1=['hi', 'heloo', 'python', 'bihari', 'hai']
+ls1.pop(2)
+print(ls1)
+
+
+list=[3,1,4,2]
+list.sort(reverse=True)
+print(list)
+
+
+list=[3,1,4,2]
+list.clear()
+print(list)
+
+
+list=[3,1,4,2]
+list=[x*2 for x in list]
+print(list)
+
+
+d={'a':'b','b':2}
+d.clear()
+print(d)
+
+
+d={'a':1,'b':2,'c':3}
+for key in d:
+  d[key]*=2
+print(d)
+
+
+d={}
+keys=['a','b','c']
+values=[1,2,3]
+for k,v in zip(keys,values):
+  d[k]=v  
+print(d)
+
+
+
+my_set={1,684,298,32465}
+print(my_set)
+
+
+my_set={1,684,298,32465}
+e=my_set.pop()
+print(e)
+print(my_set)
+
+
+my_set={1,684,298,32465}
+my_set.clear()
+print(my_set)
+
+
+s1={1,2,3}
+s2={4,5}
+s1=s1.union(s2)
+print(s1)
+
+
+s1={1,2,3}
+s2={2,3,4,5}
+s1.intersection_update(s2)
+print(s1)
+
+
+s1={1,2,3}
+s2={2,3,4,5}
+s1.difference_update(s2)
+print(s1)
+
+
+
+s1={1,2,3}
+if 4 not in s1:
+  s1.add('f6854fhfd68d468hh68sh45')
+print(s1)
+
+
+
