de5442bc6b5b6cb635c1f332e9f35f3c4f16b75f
abhiishekM <abhishivam285@gmail.com>
2025-01-29 10:59:22 +0530

    Update day9porgrams.py

--- day9porgrams.py
+++ day9porgrams.py
@@ -1,1 +1,31 @@
+packed_tpl=(98,4398,984)
+print(packed_tpl) #output (98, 4398, 984)
+a,b,c=packed_tpl
+print(a,b,c) #output 98 4398 984
 
+
+packed_list=[354,355,215,315]
+print(packed_list) #output [354, 355, 215, 315]
+a,b,c,d=packed_list
+print(a,b,c,d)  #output 354 355 215 315
+
+
+packed_stri='hi','heloo','python'
+print(packed_stri) #output ('hi', 'heloo', 'python')
+a,b,c=packed_stri
+print(a,b,c) #output hi heloo python
+
+
+packed_stri='hi','heloo','python'
+print(packed_stri) #output ('hi', 'heloo', 'python')
+a,b,c=packed_stri
+print(a,b,c) #output hi heloo python
+
+
+
+#for variable lengths
+num=[384,1,53653,345,351,1,315]
+print(num)
+a,b,c,*d=num
+print(a,b,c,*d) #output 384 1 53653 345 351 1 315
+print(a,b,c,d)  #output 384 1 53653 [345, 351, 1, 315]
