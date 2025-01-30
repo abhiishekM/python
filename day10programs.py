3b48609a39badb8fa33f6572af85d8bac07866f0
abhiishekM <abhishivam285@gmail.com>
2025-01-30 10:35:49 +0530

    Update day10programs.py

--- day10programs.py
+++ day10programs.py
@@ -1,1 +1,53 @@
+words=['python','is','easy']
+result='$'.join(words)
+print(result)
+#python$is$easy
+
+sen='my name is sujeet'
+result=sen.split(' ')
+print(result)
+#['my', 'name', 'is', 'sujeet']
+
+
+sen='  sujeet   '
+result=sen.strip()
+print(result)#sujeet
+
+
+sen='my name is sujeet'
+result=sen.replace('sujeet','sujeet R')
+print(result)
+#my name is sujeet R
+
+
+
+sen='my name is sujeet'
+result=sen.find('sujeet')
+print(result)
+#11
+
+
+sen='my name is sujeetme is sujeetme is sujeetme is sujeetme is sujeetme is sujeet'
+result=sen.count('sujeet')
+print(result)
+#6
+
+sen='my name is sujeet'
+result=sen.capitalize()
+print(result)
+#My name is sujeet
+
+
+
+sen='my name is sujeet'
+print(sen[::-1])
+#teejus si eman ym
+
+
+
+
+sen='my name is sujeet'
+lst=list(sen)
+print(len(lst))
+#17
 
